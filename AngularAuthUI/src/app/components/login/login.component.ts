9bb7d02bb2cd0b579e889691f7a4c64daac64019
higormachado05 <machado.higor98@gmail.com>
2023-12-12 21:04:28 -0300

    Adicionando pasta ao repositório sem cache

---
+++ AngularAuthUI/src/app/components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+
+  constructor(private fb: FormBuilder ) { }
+
+  ngOnInit(): void {
+  }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+}
